1425d4d7818b065ceb31047b82a27b50b6b58fa3
SamAbderholden <abderholdens@gmail.com>
2025-04-07 10:51:13 -0600

    Added video entropy

--- collectors.py
+++ collectors.py
@@ -1,6 +1,6 @@
 import cv2
 import subprocess
-import random
+import hashlib
 
 def collect_system_entropy():
     return b'SYSTEM_ENTROPY_PLACEHOLDER'
@@ -13,32 +13,22 @@ def collect_video_entropy():
     url = "https://www.youtube.com/watch?v=DHUnz4dyb54"
     cmd = ['yt-dlp', '-g', url]
 
-    try:
-        # Get the stream
-        stream_url = subprocess.check_output(cmd).decode().strip()
-    except subprocess.CalledProcessError:
-        print("Failed to retrieve stream URL.")
-        return None
-    
+    # Get the stream
+    stream_url = subprocess.check_output(cmd).decode().strip()
     # Feed it to CV2 
     cap = cv2.VideoCapture(stream_url)
-    # Byte array to hold output
-    entropy_bytes = bytearray()
-    # Grab 10 frames
-    for _ in range(10):
-        ret, frame = cap.read()
-        if not ret:
-            continue
-        
-        # Flatten that sucker down
-        pixels = frame.flatten()
-        # 100 pixel sample fo each frame
-        sampled = random.sample(list(pixels), 100)
-        # Add them to our byte array
-        entropy_bytes.extend(sampled)
+
+    # Grab frame
+    ret, frame = cap.read()
+    # Grab the pixel values
+    pixels = frame.flatten()
+    # Hash down to 256 
+    hashed = hashlib.sha256(pixels).digest()
     # Release stream URL
     cap.release()
-    return entropy_bytes
+    
+    # return our hashed vals
+    return hashed
 
 def collect_all_entropy():
     return (
